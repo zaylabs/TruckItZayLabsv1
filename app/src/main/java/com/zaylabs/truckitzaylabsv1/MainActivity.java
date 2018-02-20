d0d07b7440431db6651f0c3d9a731975b1fbeae3
Adil Raza <adilraza@gmail.com>
2018-02-20 19:10:15 +0500

    Main Activity Work

--- app/src/main/java/com/zaylabs/truckitzaylabsv1/MainActivity.java
+++ app/src/main/java/com/zaylabs/truckitzaylabsv1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.zaylabs.truckitzaylabsv1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,9 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private DatabaseReference mfirebaseDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +34,31 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mAuth = FirebaseAuth.getInstance();
+        mfirebaseDB = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "User is Signed In", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                mAuth.signOut();
+
             }
         });
 
@@ -80,17 +110,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.Profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.History) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.cargo_calculator) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.action_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout) {
+
+        } else if (id == R.id.get_help) {
 
         }
 
@@ -98,4 +128,16 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+
 }
