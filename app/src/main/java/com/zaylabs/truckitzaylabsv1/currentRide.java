eb9f084bf37c6abfd8526a3bc485c721a1cadab7
Adil Raza <adilraza@gmail.com>
2018-04-14 04:36:49 -0700

    Current Ride

--- app/src/main/java/com/zaylabs/truckitzaylabsv1/currentRide.java
+++ app/src/main/java/com/zaylabs/truckitzaylabsv1/currentRide.java
@@ -2,12 +2,79 @@ package com.zaylabs.truckitzaylabsv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import com.zaylabs.truckitzaylabsv1.DTO.acceptRequest;
+import com.zaylabs.truckitzaylabsv1.RecylerViewAdapter.currentRideAdapter;
+import java.util.List;
 
 public class currentRide extends AppCompatActivity {
+    private FirebaseFirestore firestoreDB;
+    private RecyclerView mDhistory;
+
+    private List<acceptRequest> dHistory;
+    private acceptRequest acceptRequest;
+    private currentRideAdapter currentRideAdapter;
+    private String driverID;
+    private String TAG;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_ride);
+        firestoreDB = FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+        driverID = mAuth.getCurrentUser().getUid();
+
+
+
+        dHistory = new ArrayList<>();
+        currentRideAdapter = new currentRideAdapter(this,dHistory);
+        mDhistory = (RecyclerView)findViewById(R.id.currentRideRV);
+        mDhistory.setHasFixedSize(true);
+        mDhistory.setLayoutManager(new LinearLayoutManager(this));
+        mDhistory.setAdapter(currentRideAdapter);
+
+        firestoreDB.collection("acceptRequest").whereEqualTo("cid", driverID).addSnapshotListener(new EventListener<QuerySnapshot>() {
+
+            @Override
+            public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.d(TAG, "Error:" + e.getMessage());
+                }
+                for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                    switch (doc.getType()) {
+                        case ADDED:
+                            acceptRequest = doc.getDocument().toObject(acceptRequest.class);
+                            dHistory.add(acceptRequest);
+                            currentRideAdapter.notifyDataSetChanged();
+                            break;
+                        case MODIFIED:
+                            acceptRequest = doc.getDocument().toObject(acceptRequest.class);
+                            dHistory.add(acceptRequest);
+                            currentRideAdapter.notifyDataSetChanged();
+                            break;
+                        case REMOVED:
+                            dHistory.remove(acceptRequest);
+                            currentRideAdapter.notifyDataSetChanged();
+                            break;
+                    }
+
+                }
+            }
+        });
+
     }
 }
